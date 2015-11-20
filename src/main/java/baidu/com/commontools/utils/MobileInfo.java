6a2f9fbbdea91f23966f3ba31c0bc6582ba02bee
weimuhua <weimuhua0812@126.com>
2015-11-20 13:13:23 +0800

    change : add MobileInfo.java.

---
+++ src/main/java/baidu/com/commontools/utils/MobileInfo.java
@@ -0,0 +1,22 @@
+package baidu.com.commontools.utils;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.WindowManager;
+
+public class MobileInfo {
+
+    public static DisplayMetrics getScreenMetrics(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        return dm;
+    }
+
+    public static float dp2px(Context cxt, float dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                cxt.getResources().getDisplayMetrics());
+    }
+
+}
