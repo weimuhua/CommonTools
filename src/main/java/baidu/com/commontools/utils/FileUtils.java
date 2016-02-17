8d7058f5303feb1822d94e9de94022008ccd986b
weimuhua <weimuhua0812@126.com>
2016-02-17 10:57:27 +0800

    change : add FileUtils.

---
+++ src/main/java/baidu/com/commontools/utils/FileUtils.java
@@ -0,0 +1,66 @@
+package baidu.com.commontools.utils;
+
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+public class FileUtils {
+
+    public static String readStreamAsString(InputStream is) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+        StringBuilder result = new StringBuilder();
+        String line;
+
+        boolean first = true;
+        while ((line = reader.readLine()) != null) {
+            if (!first) {
+                result.append('\n');
+            } else {
+                first = false;
+            }
+            result.append(line);
+        }
+
+        return result.toString();
+    }
+
+    public static void saveStreamToFile(InputStream is, File file) throws IOException {
+        File dirFile = file.getParentFile();
+        if (!dirFile.exists()) {
+            dirFile.mkdirs();
+        }
+
+        FileOutputStream fos = new FileOutputStream(file);
+        try {
+            byte[] buffer = new byte[1024 * 32];
+            int len;
+
+            while ((len = is.read(buffer)) != -1) {
+                fos.write(buffer, 0, len);
+            }
+            fos.flush();
+        } finally {
+            close(fos);
+            close(is);
+        }
+    }
+
+    public static void close(Closeable target) {
+        try {
+            if (target != null)
+                target.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteFile(String path, String fileName) {
+        new File(path, fileName).delete();
+    }
+}
