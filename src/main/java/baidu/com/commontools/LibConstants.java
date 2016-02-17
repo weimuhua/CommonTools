7ed91181ea8cb9b159287cd50dc4cc566c583448
weimuhua <weimuhua0812@126.com>
2016-02-17 11:02:08 +0800

    change : add ImageDownloader.

---
+++ src/main/java/baidu/com/commontools/LibConstants.java
@@ -0,0 +1,10 @@
+package baidu.com.commontools;
+
+
+import android.os.Environment;
+
+public class LibConstants {
+    public static final String APP_PATH = Environment.getExternalStorageDirectory().getPath() + "/MhToolsDir";
+
+    public static final String IMG_PATH = APP_PATH + "/img_download/";
+}
