736c3f8dcb276f6375773e58ffd0bb1be9907ffa
weimuhua <weimuhua0812@126.com>
2019-01-06 17:52:35 +0800

    add FileUtil#copy

--- src/main/java/baidu/com/commontools/utils/FileUtils.java
+++ src/main/java/baidu/com/commontools/utils/FileUtils.java
@@ -4,10 +4,12 @@ package baidu.com.commontools.utils;
 import java.io.BufferedReader;
 import java.io.Closeable;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 
 public class FileUtils {
@@ -62,5 +64,24 @@ public class FileUtils {
 
     public static void deleteFile(String path, String fileName) {
         new File(path, fileName).delete();
+    }
+
+    public static void copy(File src, File dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        try {
+            OutputStream out = new FileOutputStream(dst);
+            try {
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+            } finally {
+                out.close();
+            }
+        } finally {
+            in.close();
+        }
     }
 }
